18ec84313a0a6ff839bdf43043b610e9d3b9323b
TyannS <cth20030624@gmail.com>
2026-05-27 14:08:12 +0800

    Add invisible item frame recipe

--- src/main/java/com/tyanns/asterola/datagen/ModEnUsLangProvider.java
+++ src/main/java/com/tyanns/asterola/datagen/ModEnUsLangProvider.java
@@ -16,27 +16,27 @@ public class ModEnUsLangProvider extends FabricLanguageProvider {
     @Override
     public void generateTranslations(HolderLookup.Provider provider, TranslationBuilder translationBuilder) {
         translationBuilder.add(ModItems.ROSE_GOLD_INGOT, "Rose Gold Ingot");
-        translationBuilder.add(ModItems.ROSE_GOLDEN_SWORD,"Rose Golden Sword");
-        translationBuilder.add(ModItems.ROSE_GOLDEN_SHOVEL,"Rose Golden Shovel");
-        translationBuilder.add(ModItems.ROSE_GOLDEN_PICKAXE,"Rose Golden Pickaxe");
-        translationBuilder.add(ModItems.ROSE_GOLDEN_AXE,"Rose Golden Axe");
-        translationBuilder.add(ModItems.ROSE_GOLDEN_HOE,"Rose Golden Hoe");
-        translationBuilder.add(ModItems.ROSE_GOLDEN_SPEAR,"Rose Golden Spear");
-        translationBuilder.add(ModItems.ROSE_GOLDEN_HELMET,"Rose Golden Helmet");
-        translationBuilder.add(ModItems.ROSE_GOLDEN_CHESTPLATE,"Rose Golden Chestplate");
-        translationBuilder.add(ModItems.ROSE_GOLDEN_LEGGINGS,"Rose Golden Leggings");
-        translationBuilder.add(ModItems.ROSE_GOLDEN_BOOTS,"Rose Golden Boots");
+        translationBuilder.add(ModItems.ROSE_GOLDEN_SWORD, "Rose Golden Sword");
+        translationBuilder.add(ModItems.ROSE_GOLDEN_SHOVEL, "Rose Golden Shovel");
+        translationBuilder.add(ModItems.ROSE_GOLDEN_PICKAXE, "Rose Golden Pickaxe");
+        translationBuilder.add(ModItems.ROSE_GOLDEN_AXE, "Rose Golden Axe");
+        translationBuilder.add(ModItems.ROSE_GOLDEN_HOE, "Rose Golden Hoe");
+        translationBuilder.add(ModItems.ROSE_GOLDEN_SPEAR, "Rose Golden Spear");
+        translationBuilder.add(ModItems.ROSE_GOLDEN_HELMET, "Rose Golden Helmet");
+        translationBuilder.add(ModItems.ROSE_GOLDEN_CHESTPLATE, "Rose Golden Chestplate");
+        translationBuilder.add(ModItems.ROSE_GOLDEN_LEGGINGS, "Rose Golden Leggings");
+        translationBuilder.add(ModItems.ROSE_GOLDEN_BOOTS, "Rose Golden Boots");
         translationBuilder.add(ModItems.ROSE_GOLDEN_APPLE, "Rose Golden Apple");
-        translationBuilder.add(ModItems.EMERALD_SWORD,"Emerald Sword");
-        translationBuilder.add(ModItems.EMERALD_SHOVEL,"Emerald Shovel");
-        translationBuilder.add(ModItems.EMERALD_PICKAXE,"Emerald Pickaxe");
-        translationBuilder.add(ModItems.EMERALD_AXE,"Emerald Axe");
-        translationBuilder.add(ModItems.EMERALD_HOE,"Emerald Hoe");
-        translationBuilder.add(ModItems.EMERALD_SPEAR,"Emerald Spear");
-        translationBuilder.add(ModItems.EMERALD_HELMET,"Emerald Helmet");
-        translationBuilder.add(ModItems.EMERALD_CHESTPLATE,"Emerald Chestplate");
-        translationBuilder.add(ModItems.EMERALD_LEGGINGS,"Emerald Leggings");
-        translationBuilder.add(ModItems.EMERALD_BOOTS,"Emerald Boots");
+        translationBuilder.add(ModItems.EMERALD_SWORD, "Emerald Sword");
+        translationBuilder.add(ModItems.EMERALD_SHOVEL, "Emerald Shovel");
+        translationBuilder.add(ModItems.EMERALD_PICKAXE, "Emerald Pickaxe");
+        translationBuilder.add(ModItems.EMERALD_AXE, "Emerald Axe");
+        translationBuilder.add(ModItems.EMERALD_HOE, "Emerald Hoe");
+        translationBuilder.add(ModItems.EMERALD_SPEAR, "Emerald Spear");
+        translationBuilder.add(ModItems.EMERALD_HELMET, "Emerald Helmet");
+        translationBuilder.add(ModItems.EMERALD_CHESTPLATE, "Emerald Chestplate");
+        translationBuilder.add(ModItems.EMERALD_LEGGINGS, "Emerald Leggings");
+        translationBuilder.add(ModItems.EMERALD_BOOTS, "Emerald Boots");
         translationBuilder.add(ModItems.MUSIC_DISC_YOU_ARE_BEAUTIFUL_VOMIT_VERSION, "Music Disc");
 
         translationBuilder.add(ModBlocks.ROSE_GOLD_BLOCK, "Rose Gold Block");
@@ -45,19 +45,19 @@ public class ModEnUsLangProvider extends FabricLanguageProvider {
 
         translationBuilder.add("trim_material.asterola.rose_gold", "Rose Gold Material");
 
-        translationBuilder.add("item.asterola.music_disc_you_are_beautiful_vomit_version.description", "Tiktok@justinjmooremusic - You Are Beautiful (Vomit Version)");
-
-        translationBuilder.add("armortip.asterola.rose_gold_armortip","§6§n[SHIFT]§r§r");
-        translationBuilder.add("armortip.asterola.rose_gold_armortip.shift","§dGrants §r§d§nSpeed II§r§r§d when wearing a full set of Rose Gold Armor§r");
-        translationBuilder.add("armortip.asterola.emerald_armortip","§6§n[SHIFT]§r§r");
-        translationBuilder.add("armortip.asterola.emerald_armortip.shift","§2Grants §r§2§nHero of the Village V§r§r§2 when wearing a full set of Emerald Armor§r");
-        translationBuilder.add("tooltip.asterola.emerald_tooltip","§6§n[SHIFT]§r§r");
-        translationBuilder.add("tooltip.asterola.emerald_tooltip.shift_1","§2The glittering light of wealth multiplies all things§r");
-        translationBuilder.add("tooltip.asterola.emerald_tooltip.shift_2","§2Increases current §r§2§nFortune§r§r§2 and §r§2§nLooting§r§r§2 by 1 level§r");
+        translationBuilder.add("armortip.asterola.rose_gold_armortip", "§6§n[SHIFT]§r§r");
+        translationBuilder.add("armortip.asterola.rose_gold_armortip.shift", "§dGrants §r§d§nSpeed II§r§r§d when wearing a full set of Rose Gold Armor§r");
+        translationBuilder.add("armortip.asterola.emerald_armortip", "§6§n[SHIFT]§r§r");
+        translationBuilder.add("armortip.asterola.emerald_armortip.shift", "§2Grants §r§2§nHero of the Village V§r§r§2 when wearing a full set of Emerald Armor§r");
+        translationBuilder.add("tooltip.asterola.emerald_tooltip", "§6§n[SHIFT]§r§r");
+        translationBuilder.add("tooltip.asterola.emerald_tooltip.shift_1", "§2The glittering light of wealth multiplies all things§r");
+        translationBuilder.add("tooltip.asterola.emerald_tooltip.shift_2", "§2Increases current §r§2§nFortune§r§r§2 and §r§2§nLooting§r§r§2 by 1 level§r");
 
         translationBuilder.add("jukebox_song.asterola.you_are_beautiful_vomit_version", "Tiktok@justinjmooremusic - You Are Beautiful (Vomit Version)");
 
-        translationBuilder.add("subtitles.item.armor.equip_rose_gold","Rose gold armor clinks");
-        translationBuilder.add("subtitles.item.armor.equip_emerald","Emerald armor clanks");
+        translationBuilder.add("subtitles.item.armor.equip_rose_gold", "Rose gold armor clinks");
+        translationBuilder.add("subtitles.item.armor.equip_emerald", "Emerald armor clanks");
+
+        translationBuilder.add("item.asterola.invisible_item_frame.description", "Cloaked in the phantom's hidden power");
     }
 }
--- src/main/java/com/tyanns/asterola/datagen/ModZhCnLangProvider.java
+++ src/main/java/com/tyanns/asterola/datagen/ModZhCnLangProvider.java
@@ -2,7 +2,6 @@ package com.tyanns.asterola.datagen;
 
 import com.tyanns.asterola.block.ModBlocks;
 import com.tyanns.asterola.item.ModItems;
-import com.tyanns.asterola.sound.ModSoundEvents;
 import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricLanguageProvider;
 import net.minecraft.core.HolderLookup;
@@ -17,27 +16,27 @@ public class ModZhCnLangProvider extends FabricLanguageProvider {
     @Override
     public void generateTranslations(HolderLookup.Provider provider, TranslationBuilder translationBuilder) {
         translationBuilder.add(ModItems.ROSE_GOLD_INGOT, "玫瑰金锭");
-        translationBuilder.add(ModItems.ROSE_GOLDEN_SWORD,"玫瑰金剑");
-        translationBuilder.add(ModItems.ROSE_GOLDEN_SHOVEL,"玫瑰金锹");
-        translationBuilder.add(ModItems.ROSE_GOLDEN_PICKAXE,"玫瑰金镐");
-        translationBuilder.add(ModItems.ROSE_GOLDEN_AXE,"玫瑰金斧");
-        translationBuilder.add(ModItems.ROSE_GOLDEN_HOE,"玫瑰金锄");
-        translationBuilder.add(ModItems.ROSE_GOLDEN_SPEAR,"玫瑰金矛");
-        translationBuilder.add(ModItems.ROSE_GOLDEN_HELMET,"玫瑰金头盔");
-        translationBuilder.add(ModItems.ROSE_GOLDEN_CHESTPLATE,"玫瑰金胸甲");
-        translationBuilder.add(ModItems.ROSE_GOLDEN_LEGGINGS,"玫瑰金护腿");
-        translationBuilder.add(ModItems.ROSE_GOLDEN_BOOTS,"玫瑰金靴子");
+        translationBuilder.add(ModItems.ROSE_GOLDEN_SWORD, "玫瑰金剑");
+        translationBuilder.add(ModItems.ROSE_GOLDEN_SHOVEL, "玫瑰金锹");
+        translationBuilder.add(ModItems.ROSE_GOLDEN_PICKAXE, "玫瑰金镐");
+        translationBuilder.add(ModItems.ROSE_GOLDEN_AXE, "玫瑰金斧");
+        translationBuilder.add(ModItems.ROSE_GOLDEN_HOE, "玫瑰金锄");
+        translationBuilder.add(ModItems.ROSE_GOLDEN_SPEAR, "玫瑰金矛");
+        translationBuilder.add(ModItems.ROSE_GOLDEN_HELMET, "玫瑰金头盔");
+        translationBuilder.add(ModItems.ROSE_GOLDEN_CHESTPLATE, "玫瑰金胸甲");
+        translationBuilder.add(ModItems.ROSE_GOLDEN_LEGGINGS, "玫瑰金护腿");
+        translationBuilder.add(ModItems.ROSE_GOLDEN_BOOTS, "玫瑰金靴子");
         translationBuilder.add(ModItems.ROSE_GOLDEN_APPLE, "玫瑰金苹果");
-        translationBuilder.add(ModItems.EMERALD_SWORD,"绿宝石剑");
-        translationBuilder.add(ModItems.EMERALD_SHOVEL,"绿宝石锹");
-        translationBuilder.add(ModItems.EMERALD_PICKAXE,"绿宝石镐");
-        translationBuilder.add(ModItems.EMERALD_AXE,"绿宝石斧");
-        translationBuilder.add(ModItems.EMERALD_HOE,"绿宝石锄");
-        translationBuilder.add(ModItems.EMERALD_SPEAR,"绿宝石矛");
-        translationBuilder.add(ModItems.EMERALD_HELMET,"绿宝石头盔");
-        translationBuilder.add(ModItems.EMERALD_CHESTPLATE,"绿宝石胸甲");
-        translationBuilder.add(ModItems.EMERALD_LEGGINGS,"绿宝石护腿");
-        translationBuilder.add(ModItems.EMERALD_BOOTS,"绿宝石靴子");
+        translationBuilder.add(ModItems.EMERALD_SWORD, "绿宝石剑");
+        translationBuilder.add(ModItems.EMERALD_SHOVEL, "绿宝石锹");
+        translationBuilder.add(ModItems.EMERALD_PICKAXE, "绿宝石镐");
+        translationBuilder.add(ModItems.EMERALD_AXE, "绿宝石斧");
+        translationBuilder.add(ModItems.EMERALD_HOE, "绿宝石锄");
+        translationBuilder.add(ModItems.EMERALD_SPEAR, "绿宝石矛");
+        translationBuilder.add(ModItems.EMERALD_HELMET, "绿宝石头盔");
+        translationBuilder.add(ModItems.EMERALD_CHESTPLATE, "绿宝石胸甲");
+        translationBuilder.add(ModItems.EMERALD_LEGGINGS, "绿宝石护腿");
+        translationBuilder.add(ModItems.EMERALD_BOOTS, "绿宝石靴子");
         translationBuilder.add(ModItems.MUSIC_DISC_YOU_ARE_BEAUTIFUL_VOMIT_VERSION, "音乐唱片");
 
         translationBuilder.add(ModBlocks.ROSE_GOLD_BLOCK, "玫瑰金块");
@@ -46,19 +45,19 @@ public class ModZhCnLangProvider extends FabricLanguageProvider {
 
         translationBuilder.add("trim_material.asterola.rose_gold", "玫瑰金质");
 
-        translationBuilder.add("item.asterola.music_disc_you_are_beautiful_vomit_version.description", "Tiktok@justinjmooremusic - You Are Beautiful（呕吐版）");
-
-        translationBuilder.add("armortip.asterola.rose_gold_armortip","§6§n[SHIFT]§r§r");
-        translationBuilder.add("armortip.asterola.rose_gold_armortip.shift","§d当装备全套玫瑰金盔甲时，将会获得§r§d§n迅捷II§r§r§d的效果§r");
-        translationBuilder.add("armortip.asterola.emerald_armortip","§6§n[SHIFT]§r§r");
-        translationBuilder.add("armortip.asterola.emerald_armortip.shift","§2当装备全套绿宝石盔甲时，将会获得§r§2§n村庄英雄V§r§r§2的效果§r");
-        translationBuilder.add("tooltip.asterola.emerald_tooltip","§6§n[SHIFT]§r§r");
-        translationBuilder.add("tooltip.asterola.emerald_tooltip.shift_1","§2闪烁的财富之光让万物倍增§r");
-        translationBuilder.add("tooltip.asterola.emerald_tooltip.shift_2","§2使当前的§r§2§n时运§r§r§2与§r§2§n抢夺§r§r§2等级提升1级§r");
+        translationBuilder.add("armortip.asterola.rose_gold_armortip", "§6§n[SHIFT]§r§r");
+        translationBuilder.add("armortip.asterola.rose_gold_armortip.shift", "§d当装备全套玫瑰金盔甲时，将会获得§r§d§n迅捷II§r§r§d的效果§r");
+        translationBuilder.add("armortip.asterola.emerald_armortip", "§6§n[SHIFT]§r§r");
+        translationBuilder.add("armortip.asterola.emerald_armortip.shift", "§2当装备全套绿宝石盔甲时，将会获得§r§2§n村庄英雄V§r§r§2的效果§r");
+        translationBuilder.add("tooltip.asterola.emerald_tooltip", "§6§n[SHIFT]§r§r");
+        translationBuilder.add("tooltip.asterola.emerald_tooltip.shift_1", "§2闪烁的财富之光让万物倍增§r");
+        translationBuilder.add("tooltip.asterola.emerald_tooltip.shift_2", "§2使当前的§r§2§n时运§r§r§2与§r§2§n抢夺§r§r§2等级提升1级§r");
 
         translationBuilder.add("jukebox_song.asterola.you_are_beautiful_vomit_version", "Tiktok@justinjmooremusic - You Are Beautiful（呕吐版）");
 
-        translationBuilder.add("subtitles.item.armor.equip_rose_gold","玫瑰金盔甲：叮当");
-        translationBuilder.add("subtitles.item.armor.equip_emerald","绿宝石盔甲：铿锵");
+        translationBuilder.add("subtitles.item.armor.equip_rose_gold", "玫瑰金盔甲：叮当");
+        translationBuilder.add("subtitles.item.armor.equip_emerald", "绿宝石盔甲：铿锵");
+
+        translationBuilder.add("item.asterola.invisible_item_frame.description", "披上幻翼的隐匿之力");
     }
 }
